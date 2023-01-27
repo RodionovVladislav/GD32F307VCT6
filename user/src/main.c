cf73a312927164ff8d945f81ffcbce8252ca7a33
RodionovVladislav <119339253+RodionovVladislav@users.noreply.github.com>
2023-01-27 18:47:19 +0300

    №1

--- user/src/main.c
+++ user/src/main.c
@@ -3,10 +3,34 @@
 #include "gd32f30x_rcu.h"               // GigaDevice::Device:GD32F30x_StdPeripherals:RCU
 #include <stdint.h>
 
-void Delay (void)
+#define DEVICE_ID (uint32_t 0x)
+
+#define DELAY			100
+#define FAST			1
+#define SLLOW			4
+
+uint8_t g_speed = FAST;
+uint16_t timer_pr = 0xFFFF;
+uint16_t timer_dt = 0x0001;
+uint16_t timer_dt_inc = 0x008F;
+uint16_t presceler_value = 0;
+timer_channel_output_config timer_oc_init_structure;
+
+void button_exint_init(void)
 {
-	
+  
 }
+
+void button_isr(void)
+{
+
+}
+
+void EXINT0_IRQHandler(void)
+{
+  button_isr();
+}
+
 
 int main(void) 
 {
@@ -16,7 +40,7 @@ int main(void)
 	while(1)
 	{
 	  
-		Delay();
+		
 		
 	}
 }
